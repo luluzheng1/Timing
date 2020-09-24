0199dd7ee3c14290e5b7a06ff0d8202bde01f333
Lulu <lulu.zheng@tufts.edu>
2020-09-23 21:50:28 -0400

    sempost_calculation

--- timing.c
+++ timing.c
@@ -14,11 +14,12 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <fcntl.h>
+#include <errno.h>
 
 typedef enum
 {
     million,
-    hundred_million,
+    hundred_million = 100000000,
     billion
 } cycles;
 
@@ -55,6 +56,15 @@ static inline double get_seconds(time_t start_sec, time_t end_sec, long start_ns
     return (double)seconds + (double)ns / (double)BILLION;
 }
 
+// compute the total seconds for getrusage
+static inline double get_seconds_r(long int start_sec, long int end_sec, long int start_usec, long int end_usec)
+{
+    long int seconds = end_sec - start_sec;
+    long int us = end_usec - start_usec;
+
+    return (double)seconds + (double)us / (double)MILLION;
+}
+
 // calculate time to run empty loop x times
 static inline double get_emptyloop_time(cycles c)
 {
@@ -84,7 +94,8 @@ static inline double get_emptyloop_time(cycles c)
     return get_seconds(start.tv_sec, finish.tv_sec, start.tv_nsec, finish.tv_nsec);
 }
 
-static inline void get_sempost_time(result *t)
+// calculating time for sempost using clock-gettime
+static inline void get_sempost_time(result *t, double lt)
 {
     struct timespec start, finish;
     // how to manipulate semaphores
@@ -99,16 +110,80 @@ static inline void get_sempost_time(result *t)
         success = sem_post(&onesem);
     }
     clock_gettime(CLOCK_REALTIME, &finish);
+    if (sem_destroy(&onesem) < 0)
+    {
+        fprintf(stderr, "Error: %d\n", errno);
+        exit(errno);
+    }
 
-    double seconds = get_seconds(start.tv_sec, finish.tv_sec, start.tv_nsec, finish.tv_nsec) - get_emptyloop_time(hundred_million);
+    double seconds = get_seconds(start.tv_sec, finish.tv_sec, start.tv_nsec, finish.tv_nsec) - lt;
     t->utime = seconds;
-
-    printf("total seconds: %e\n", seconds);
 }
 
+// calculating time for sempost using getrusage
+static inline void get_sempost_usage(result *t)
+{
+    struct rusage start, finish;
+    // how to manipulate semaphores
+    sem_t onesem;
+    int success = sem_init(&onesem, 0, 0);
+    if (success < 0)
+        perror("sem_init");
+
+    getrusage(RUSAGE_SELF, &start);
+    for (int i = 0; i < HUNDRED_MILLION; i++)
+    {
+        success = sem_post(&onesem);
+    }
+    getrusage(RUSAGE_SELF, &finish);
+    if (sem_destroy(&onesem) < 0)
+    {
+        fprintf(stderr, "Error: %d\n", errno);
+        exit(errno);
+    }
+
+    double seconds = get_seconds_r(start.ru_utime.tv_sec, finish.ru_utime.tv_sec, start.ru_utime.tv_usec, finish.ru_utime.tv_usec) - get_emptyloop_time(hundred_million);
+    t->utime = seconds;
+}
+
+// caculate the time for a single function call using clock_gettime
+static inline void calc_ttime(result *times, int size, int c, char *fname)
+{
+
+    double sum_utime = 0.0;
+    double avg_utime = 0.0;
+    double sum_stime = 0.0;
+    double avg_stime = 0.0;
+    for (int i = 0; i < size; i++)
+    {
+        sum_utime = sum_utime + times[i].utime;
+        sum_stime = sum_stime + times[i].stime;
+    }
+
+    avg_utime = sum_utime / (double)size / (double)c;
+    avg_stime = sum_stime / (double)size / (double)c;
+    printf("avg user time for %s: %e seconds\n", fname, avg_utime);
+    printf("avg system time for %s: %e seconds\n", fname, avg_stime);
+}
+
+// test time to run sempost over 10 trials
+static inline void test_sempost(result *times)
+{
+    double loop_time = get_emptyloop_time(hundred_million);
+    for (int i = 0; i < 10; i++)
+    {
+        times[i].utime = 0.0;
+        times[i].stime = 0.0;
+        get_sempost_time(&times[i], loop_time);
+        // printf("trial %d: total user time: %e seconds\n", i, times[i].utime);
+        // printf("trial %d: total system time: %e seconds\n", i, times[i].stime);
+    }
+    calc_ttime(times, 10, hundred_million, "sem_post");
+}
 int main()
 {
-    result sempost_time = {.utime = 0.0, .stime = 0.0};
+    result sempost_time[10];
+    test_sempost(sempost_time);
 
     // how to invoke mutex locks
     pthread_mutex_t onelock;
@@ -116,7 +191,6 @@ int main()
     pthread_mutex_lock(&onelock);
     pthread_mutex_unlock(&onelock);
 
-    get_sempost_time(&sempost_time);
     // how to open a file
     int fd = open("/etc/motd", O_RDONLY);
     // did it work?
